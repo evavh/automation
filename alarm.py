4f87e7af36e0534ef14c43e8978dfec8f761db08
BuildTools <informaticavia@users.noreply.github.com>
2016-08-23 13:16:24 +0200

    Added travel time calculation to alarm script

--- alarm.py
+++ alarm.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+import requests
+import datetime
+import math
+
 import google_api
 from config import alarm_config
 
@@ -12,7 +16,20 @@ if __name__ == '__main__':
     expanded_location = location
     for key in known_locations:
         if location.startswith(key):
-            expanded_location = known_locations[location]
+            expanded_location = known_locations[key]
             break
-
-    print(expanded_location)
+    
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+    parameters = {'mode':"walking", 'origins':known_locations['Thuis'], 'destinations':expanded_location, 'key':alarm_config.api_key}
+    r = requests.get(url, params=parameters)
+    distance_matrix = r.json()
+    
+    travel_seconds = distance_matrix['rows'][0]['elements'][0]['duration']['value']/3
+    travel_minutes = round(travel_seconds/60)
+    travel_time = datetime.timedelta(seconds=travel_seconds)
+    
+    print("Next event on the calendar is {} on {:%d-%m at %H:%M}".format(first_event['name'], first_event['start']))
+    if travel_minutes > 60:
+        print("Time to travel from home to {}: {} hours and {} minutes".format(expanded_location, math.floor(travel_minutes/60), travel_minutes%60))
+    else:
+        print("Time to travel from home to {}: {} minutes".format(expanded_location, travel_minutes))
