e2f4f20e8e9f17e82373ae7f869b1d67fd56e283
BuildTools <informaticavia@users.noreply.github.com>
2016-12-28 18:41:18 +0100

    cleanup

--- alarm.py
+++ alarm.py
@@ -5,14 +5,14 @@
 import requests
 import datetime
 import math
+import os
 from crontab import CronTab
 
 import google_api
 import music
 from config import alarm_config
 
-SLEEP_HOURS = 10
-WAKEUP_PLAYLIST = "wakeup"
+THIS_FILE = os.path.join(os.path.dirname(__file__), "alarm.py")
 
 def first_event_timing():
     first_event = google_api.first_event()
@@ -23,16 +23,16 @@ def first_event_timing():
     if "location" in first_event:
         location = first_event['location']
         
-        known_locations = alarm_config.known_locations
+        KNOWN_LOCATIONS = alarm_config.KNOWN_LOCATIONS
         
         expanded_location = location
-        for key in known_locations:
+        for key in KNOWN_LOCATIONS:
             if location.startswith(key):
-                expanded_location = known_locations[key]
+                expanded_location = KNOWN_LOCATIONS[key]
                 break
         
         url = "https://maps.googleapis.com/maps/api/distancematrix/json"
-        parameters = {'mode':"walking", 'origins':known_locations['Thuis'], 'destinations':expanded_location, 'key':alarm_config.api_key}
+        parameters = {'mode':"walking", 'origins':KNOWN_LOCATIONS['Thuis'], 'destinations':expanded_location, 'key':alarm_config.API_KEY}
         r = requests.get(url, params=parameters)
         distance_matrix = r.json()
         
@@ -45,7 +45,7 @@ def first_event_timing():
 
 def alarm_time():
     timing = first_event_timing()
-    sleep_delta = datetime.timedelta(hours=SLEEP_HOURS)
+    SLEEP_DELTA = datetime.timedelta(hours=alarm_config.SLEEP_HOURS)
     if timing:
         first_event, travel_time = first_event_timing()
         routine = datetime.timedelta(minutes=45)
@@ -55,19 +55,19 @@ def alarm_time():
         getup_time = first_event['start'] - total_time
         
         #if there is plenty of time to sleep
-        if getup_time - datetime.datetime.now() > sleep_delta:
-            return datetime.datetime.now() + sleep_delta
+        if getup_time - datetime.datetime.now() > SLEEP_DELTA:
+            return datetime.datetime.now() + SLEEP_DELTA
         else:
             return getup_time
     else:
-        return datetime.datetime.now() + sleep_delta
+        return datetime.datetime.now() + SLEEP_DELTA
 
 def set_cron_alarm(alarm_time):
     my_cron = CronTab(user=True) #load my crontab
     my_cron.remove_all(comment="automatic_alarm") #clean up old entries
     
     #create and setup new job
-    job = my_cron.new(command='/home/eva/server/alarm.py', comment="automatic_alarm")
+    job = my_cron.new(command=THIS_FILE, comment="automatic_alarm")
     job.hour.on(alarm_time.hour)
     job.minute.also.on(alarm_time.minute)
     
@@ -79,6 +79,6 @@ def clear_alarm():
     my_cron.write() #write the changes to the crontab
 
 if __name__ == '__main__':
-    music.start_shuffle_playlist(WAKEUP_PLAYLIST)
+    music.start_shuffle_playlist(alarm_config.WAKEUP_PLAYLIST)
     commands = {'command': 'night_off'}
-    requests.post("192.168.0.111:8080", params=commands)
+    requests.post(alarm_config.LOCAL_URL, params=commands)
