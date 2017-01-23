e3da0fc7f1abc1c9daa8fdcad7aec9f5655a9947
BuildTools <informaticavia@users.noreply.github.com>
2017-01-23 13:43:25 +0100

    changed hue library from phue to qhue, still needs real-life testing

--- lamp_control.py
+++ lamp_control.py
@@ -5,40 +5,44 @@
 #temp is colour temperature, bright is brightness
 
 import datetime
-import phue
+import qhue
+import time
 
 from config import *
 
-BRIDGE = phue.Bridge(BRIDGE_IP)
-LAMPS = BRIDGE.get_light_objects()
+BRIDGE = qhue.Bridge(BRIDGE_IP, BRIDGE_USERNAME)
+LAMPS = BRIDGE.lights()
 
 def lamp_probe():
-    for lamp in LAMPS:
-        number = lamp.light_id
-        name = lamp.name
-        on = lamp.on
-        if on:
-            colortemp_k = lamp.colortemp_k
-            bright = lamp.brightness
-            xy = lamp.xy
-            print("Lamp {} ({}) is set to {}K, colour {}, at brightness {}.".format(number, name, colortemp_k, xy, bright))
+    for lamp_num in LAMPS:
+        lamp = BRIDGE.lights[lamp_num]()
+        name = lamp['name']
+        if lamp['state']['on']:
+            temp_mir = lamp['state']['ct']
+            temp_k = int(1000000 / temp_mir)
+            bright = lamp['state']['bri']
+            xy = lamp['state']['xy']
+            print("Lamp {} ({}) is set to {}K, colour {}, at brightness {}.".format(lamp_num, name, temp_k, xy, bright))
         else:
-            print("Lamp {} ({}) is off.".format(number, name))
+            print("Lamp {} ({}) is off.".format(lamp_num, name))
 
-def set_to_temp(temp, bright, trans_time):
-    for lamp in LAMPS:
-        lamp.on = True
-        lamp.colortemp_k = temp
-        lamp.brightness = 1
-        BRIDGE.set_light(lamp.light_id, 'bri', bright, transitiontime=trans_time)
+def set_to_temp(temp_k, bright, trans_time_s=0.4):
+    trans_time_ds = 10*trans_time_s
+    temp_mir = int(1000000 / temp_k)
+    for lamp_num in LAMPS:
+        BRIDGE.lights[lamp_num].state(on=True)
+        BRIDGE.lights[lamp_num].state(ct=temp_mir, bri=bright, transitiontime=trans_time_ds)
     
-    return temp, bright
+    return temp_k, bright
+
+def set_off():
+    for lamp_num in LAMPS:
+        BRIDGE.lights[lamp_num].state(on=False)
 
 def set_to_xy(x, y, bright):
-    for lamp in LAMPS:
-        lamp.on = True
-        lamp.xy = (x, y)
-        lamp.brightness = bright
+    for lamp_num in LAMPS:
+        BRIDGE.lights[lamp_num].state(on=True)
+        BRIDGE.lights[lamp_num].state(xy=(x, y), bri=bright)
 
 def auto_value_now():
     hour = datetime.datetime.now().hour
@@ -61,18 +65,16 @@ def is_override():
     hour = datetime.datetime.now().hour
     minute = datetime.datetime.now().minute
     time_to_check = datetime.time(hour, minute)
-    for lamp in LAMPS:
-        if lamp.on:
+    for lamp_num in LAMPS:
+        lamp = BRIDGE.lights[lamp_num]()
+        if lamp['state']['on']:
             auto_temp, auto_bright = auto_value_now()
-            lamp_temp = lamp.colortemp_k
-            lamp_bright = lamp.brightness
-            if abs(lamp_temp-auto_temp) > 100 or abs(lamp_bright-auto_bright) > 10:
+            lamp_temp_mir = lamp['state']['ct']
+            lamp_temp_k = int(1000000 / temp_mir)
+            lamp_bright = lamp['state']['bri']
+            if abs(lamp_temp_k-auto_temp) > 100 or abs(lamp_bright-auto_bright) > 10:
                 return True #at least one lamp does not have auto's values -> not auto
     return False
-
-def set_off():
-    for lamp in LAMPS:
-        lamp.on = False
 
 def night_light_on():
     set_to_xy(0.675, 0.322, 1)
